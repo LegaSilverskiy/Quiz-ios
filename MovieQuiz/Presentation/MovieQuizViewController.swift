bafa3aa23a01b3c66a985d5a61a714966862d609
LegaSilverskiy <slepchenko98@yandex.ru>
2023-10-11 20:34:09 +0500

    Убрал лишние комменты. Исправил cornerRadius с 6 на 20. Добавил выключение кнопок после нажатия на одну из них до следующего вопроса

--- MovieQuiz/Presentation/MovieQuizViewController.swift
+++ MovieQuiz/Presentation/MovieQuizViewController.swift
@@ -2,30 +2,30 @@ import UIKit
 
 final class MovieQuizViewController: UIViewController {
     // MARK: - Lifecycle
-    struct QuizQuestion {
-      // строка с названием фильма,
+    private struct QuizQuestion {
+      /// строка с названием фильма,
       let image: String
-      // строка с вопросом о рейтинге фильма
+      /// строка с вопросом о рейтинге фильма
       let text: String
-      // булевое значение (true, false), правильный ответ на вопрос
+      /// булевое значение (true, false), правильный ответ на вопрос
       let correctAnswer: Bool
     }
-    // вью модель для состояния "Вопрос показан"
-    struct QuizStepViewModel {
-      // картинка с афишей фильма с типом UIImage
+    /// вью модель для состояния "Вопрос показан"
+   private struct QuizStepViewModel {
+      /// картинка с афишей фильма с типом UIImage
       let image: UIImage
-      // вопрос о рейтинге квиза
+      /// вопрос о рейтинге квиза
       let question: String
-      // строка с порядковым номером этого вопроса (ex. "1/10")
+     /// строка с порядковым номером этого вопроса (ex. "1/10")
       let questionNumber: String
     }
-    // для состояния "Результат квиза"
-    struct QuizResultsViewModel {
-      // строка с заголовком алерта
+    /// для состояния "Результат квиза"
+   private struct QuizResultsViewModel {
+      /// строка с заголовком алерта
       let title: String
-      // строка с текстом о количестве набранных очков
+      /// строка с текстом о количестве набранных очков
       let text: String
-      // текст для кнопки алерта
+      /// текст для кнопки алерта
       let buttonText: String
     }
     private var currentQuestionIndex = 0
@@ -73,23 +73,23 @@ final class MovieQuizViewController: UIViewController {
                 correctAnswer: false)
         ]
     
-    // метод конвертации, который принимает моковый вопрос и возвращает вью модель для экрана вопроса
+    /// метод конвертации, который принимает моковый вопрос и возвращает вью модель для экрана вопроса
     private func convert(model: QuizQuestion) -> QuizStepViewModel {
         let questionStep = QuizStepViewModel(
             image: UIImage(named: model.image) ?? UIImage(),
-            question: model.text, // 3
+            question: model.text,
             questionNumber: "\(currentQuestionIndex + 1)/\(questions.count)")
         return questionStep
     }
-    // приватный метод вывода на экран вопроса, который принимает на вход вью модель вопроса и ничего не возвращает
+    /// приватный метод вывода на экран вопроса, который принимает на вход вью модель вопроса и ничего не возвращает
     private func show(quiz step: QuizStepViewModel) {
       imageView.image = step.image
       textLabel.text = step.question
       counterLabel.text = step.questionNumber
     }
     
-    // приватный метод для показа результатов раунда квиза
-    // принимает вью модель QuizResultsViewModel и ничего не возвращает
+    /// приватный метод для показа результатов раунда квиза
+    /// принимает вью модель QuizResultsViewModel и ничего не возвращает
     private func show(quiz result: QuizResultsViewModel) {
         let alert = UIAlertController(
             title: result.title,
@@ -110,6 +110,8 @@ final class MovieQuizViewController: UIViewController {
         self.present(alert, animated: true, completion: nil)
     }
     
+    @IBOutlet weak var yesButtonAnswer: UIButton!
+    @IBOutlet weak var noButtonAnswer: UIButton!
     @IBOutlet private var imageView: UIImageView!
     @IBOutlet private var textLabel: UILabel!
     @IBOutlet private var counterLabel: UILabel!
@@ -123,12 +125,12 @@ final class MovieQuizViewController: UIViewController {
         let firstQuestion = self.questions[self.currentQuestionIndex]
         let viewModel = self.convert(model: firstQuestion)
         self.show(quiz: viewModel)
-        imageView.layer.cornerRadius = 6 // радиус скругления углов рамки
+        imageView.layer.cornerRadius = 20 /// радиус скругления углов рамки
         
     }
     
-    // приватный метод, который меняет цвет рамки
-    // принимает на вход булевое значение и ничего не возвращает
+    /// приватный метод, который меняет цвет рамки
+    /// принимает на вход булевое значение и ничего не возвращает
     private func showAnswerResult(isCorrect: Bool) {
         if isCorrect {
               correctAnswers += 1
@@ -137,15 +139,17 @@ final class MovieQuizViewController: UIViewController {
           imageView.layer.masksToBounds = true
           imageView.layer.borderWidth = 8
           imageView.layer.borderColor = isCorrect ? UIColor.ypGreen.cgColor : UIColor.ypRed.cgColor
-          
+            
           DispatchQueue.main.asyncAfter(deadline: .now() + 1.0) {
               self.showNextQuestionOrResults()
           }
+        noButtonAnswer.isEnabled = false
+        yesButtonAnswer.isEnabled = false
         
     }
     
-    // приватный метод, который содержит логику перехода в один из сценариев
-    // метод ничего не принимает и ничего не возвращает
+    /// приватный метод, который содержит логику перехода в один из сценариев
+    /// метод ничего не принимает и ничего не возвращает
     private func showNextQuestionOrResults() {
         imageView.layer.borderWidth = 0
         if currentQuestionIndex == questions.count - 1 {
@@ -161,6 +165,8 @@ final class MovieQuizViewController: UIViewController {
             let viewModel = convert(model: nextQuestion)
             
             show(quiz: viewModel)
+            noButtonAnswer.isEnabled = true
+            yesButtonAnswer.isEnabled = true
         }
         
     }
